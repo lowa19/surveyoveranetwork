fb2b180efde44657c0bc45b0a9258fd94e3a5aea
lowa19 <lowa19@up.edu>
2018-04-22 19:25:41 -0700

    Update CS445project.R

--- CS445project.R
+++ CS445project.R
@@ -19,3 +19,7 @@ df <- data.frame("1725847", "computer science", "engineering", "junior", 18, 130
 names(df) <- c("studentID", "major", "school_major", "year", "credits_current", "credits_taken", "gpa", "percentile_major", "percentile_school", "percentile_year", "athlete_status") 
 #sqlSave(con, df, tablename = "student", append = TRUE, rownames = TRUE, colnames = FALSE, verbose = FALSE, safer = TRUE,  test = TRUE)
 #sqlUpdate(con, df, tablename = "student", index = NULL, verbose = FALSE, test = FALSE, nastring = NULL, fast = TRUE)
+
+#CODE TO READ FROM A CSV FILE
+inputs <- read.csv(file = "test.csv", header = TRUE, sep = ",")
+inputs <- data.frame(inputs) #convert to a data frame
