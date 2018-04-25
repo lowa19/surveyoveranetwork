1b7e6c3d1505083bf5c46c63a0f4e52edee4159e
Adrian Low <lowa19@up.edu>
2018-04-25 08:23:08 -1000

    data pushes to database now

--- pushData.R
+++ pushData.R
@@ -3,15 +3,13 @@ require(RODBC)
 con <- odbcConnect("CS445 ODBC data source", uid="saon", pwd="CS445project")
 
 #CODE TO READ FROM A CSV FILE
-inputs <- read.csv(file = "test.csv", header = TRUE, sep = ",")
+inputs <- read.csv(file = "test.csv", header = TRUE, sep = ",") #need to change file path later
 inputs <- data.frame(inputs) #convert to a data frame
 
-#PUSH NEW DATA TO THE DATABASE
 #sample data frame
-df <- data.frame("1725847", "computer science", "engineering", "junior", 18, 130, 3.2, .7, .6, .8, 1)
+df <- data.frame(1725847, "computer science", "engineering", "junior", 18, 130, 3.2, .7, .6, .8, 1)
 names(df) <- c("studentID", "major", "school_major", "year", "credits_current", "credits_taken", "gpa", "percentile_major", "percentile_school", "percentile_year", "athlete_status") 
-#sqlSave(con, df, tablename = "student", append = TRUE, rownames = TRUE, colnames = FALSE, verbose = FALSE, safer = TRUE,  test = TRUE)
-#sqlUpdate(con, df, tablename = "student", index = NULL, verbose = FALSE, test = FALSE, nastring = NULL, fast = TRUE)
-testQuery <- paste("INSERT INTO student VALUES", df)
-testQuery
-sqlQuery(con, testQuery)
+
+#PUSH NEW DATA TO THE DATABASE
+sqlSave(con, df, tablename = "student", append = TRUE, rownames = FALSE) #change df later to the csv data frame
+
