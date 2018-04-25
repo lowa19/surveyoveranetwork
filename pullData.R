dbf0c834d80382eb375f677ef7fa5d407e64a27c
Adrian Low <lowa19@up.edu>
2018-04-25 10:30:36 -1000

    push

--- pullData.R
+++ pullData.R
@@ -1,13 +1,43 @@
-#use RODBC package
+#packages
 require(RODBC)
-require(ggplot2)
-require(reshape2)
+require(gridExtra)
+require(htmlwidgets)
+require(plotly)
+require(plyr)
 #connect to the database
 con <- odbcConnect("CS445 ODBC data source", uid="saon", pwd="CS445project")
 #returns the table as a data.frame with 11 variables/columns
-allData <- sqlQuery(con, "SELECT * FROM student")
+testdf <- sqlQuery(con, "SELECT * FROM student")
 
-#CREATE GRAPH AND EXPORT TO PNG
-plot(allData$major, allData$gpa)
-dev.copy(png, "graph.png")
-dev.off()
+# Grouped bar chart with average GPA per school/year
+freshman<- testdf[testdf$year == "freshman",]
+FRgpa<- aggregate(as.numeric(freshman$gpa), list(freshman$school), mean)
+sophomore<- testdf[testdf$year == "sophomore",]
+SOgpa<- aggregate(as.numeric(sophomore$gpa), list(sophomore$school), mean)
+junior<- testdf[testdf$year == "junior",]
+JUgpa<- aggregate(as.numeric(junior$gpa), list(junior$school), mean)
+senior<- testdf[testdf$year == "senior",]
+SEgpa<- aggregate(as.numeric(senior$gpa), list(senior$school), mean)
+
+schools<- unique(testdf$school)
+yeardf<- data.frame(schools, FRgpa$x, SOgpa$x, JUgpa$x, SEgpa$x)
+groupbar <- plot_ly(yeardf, x = schools, y = ~FRgpa$x, type = 'bar', name = 'Freshman') %>%
+  add_trace(y = ~SOgpa$x, name = 'Sophomore') %>%
+  add_trace(y = ~JUgpa$x, name = 'Junior') %>%
+  add_trace(y = ~SEgpa$x, name = 'Senior') %>%
+  layout(yaxis = list(title = 'Average GPA'), barmode = 'group')
+
+# Histogram using plotly
+hist <- plot_ly(x = testdf$gpa, type = "histogram")
+
+# Plot pie chart using plotly
+pie <- plot_ly(testdf, labels = count(testdf$major)$x, values = count(testdf$major)$freq, type = 'pie') %>%
+  layout(title = 'People with major filling out the survey',
+         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+# Export a single widget to html
+# htmlwidgets::saveWidget(as_widget(p), selfcontained = TRUE, file = "~/Google Drive/Studie/MBA 2 2017-2018/Computer Networks and Internetworking/SAoN/graph.html")
+
+# Export multiple widgets to html
+htmltools::save_html(list(as_widget(pie), as_widget(hist), as_widget(groupbar)), file = "~/Google Drive/Studie/MBA 2 2017-2018/Computer Networks and Internetworking/SAoN/graph.html")
--- pushData.R
+++ pushData.R
@@ -7,8 +7,8 @@ inputs <- read.csv(file = "test.csv", header = TRUE, sep = ",") #need to change 
 inputs <- data.frame(inputs) #convert to a data frame
 
 #sample data frame
-df <- data.frame(1725847, "computer science", "engineering", "junior", 18, 130, 3.2, .7, .6, .8, 1)
-names(df) <- c("studentID", "major", "school_major", "year", "credits_current", "credits_taken", "gpa", "percentile_major", "percentile_school", "percentile_year", "athlete_status") 
+testdf <- data.frame(1111416, "psychology", "arts and sciences", "junior", 17, 100, 4.0, .5, .4, .3, 1)
+names(testdf) <- c("studentID", "major", "school_major", "year", "credits_current", "credits_taken", "gpa", "percentile_major", "percentile_school", "percentile_year", "athlete_status") 
 
 #PUSH NEW DATA TO THE DATABASE
-sqlSave(con, df, tablename = "student", append = TRUE, rownames = FALSE) #change df later to the csv data frame
+sqlSave(con, testdf, tablename = "student", append = TRUE, rownames = FALSE) #change df later to the csv data frame
