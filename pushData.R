3875165cec49731199fb63aca2ad6e8778264b59
Adrian Low <lowa19@up.edu>
2018-04-27 09:10:56 -1000

    completed

--- pushData.R
+++ pushData.R
@@ -3,12 +3,10 @@ require(RODBC)
 con <- odbcConnect("CS445 ODBC data source", uid="saon", pwd="CS445project")
 
 #CODE TO READ FROM A CSV FILE
-inputs <- read.csv(url("https://www.something.com"), header = TRUE, sep = ",") #need to change file path later
+myUrl <- 'http://ec2-52-26-167-184.us-west-2.compute.amazonaws.com/efs-mount-point/SAoN/results/surveyResults1.csv'
+download.file(myUrl, destfile = 'C:\\Users\\Kaleo\\Documents\\GitHub\\surveyoveranetwork\\test.csv')
+inputs <- read.csv('C:\\Users\\Kaleo\\Documents\\GitHub\\surveyoveranetwork\\test.csv', header = TRUE, sep = ",") #need to change file path later
 inputs <- data.frame(inputs) #convert to a data frame
 
-#sample data frame
-testdf <- data.frame(1111416, "psychology", "arts and sciences", "junior", 17, 100, 4.0, .5, .4, .3, 1)
-names(testdf) <- c("studentID", "major", "school_major", "year", "credits_current", "credits_taken", "gpa", "percentile_major", "percentile_school", "percentile_year", "athlete_status") 
-
 #PUSH NEW DATA TO THE DATABASE
-sqlSave(con, testdf, tablename = "student", append = TRUE, rownames = FALSE) #change df later to the csv data frame
+sqlSave(con, inputs, tablename = "student", append = TRUE, rownames = FALSE)
